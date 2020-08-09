dd8518e0b0efe15b6d30c463c71e8b8b91ffae0e
Chris Fowles <chris@prosus.io>
2020-08-09 16:17:11 +1000

    output timestamp

--- main.tf
+++ main.tf
@@ -1,9 +1,17 @@
+locals {
+    timestamp = formatdate("YYYYMMDDhhmm", timestamp())
+}
+
 resource random_pet random_string {
   length    = 3
   separator = " "
   keepers = {
-      timestamp = formatdate("YYYYMMDDhhmm", timestamp())
+      timestamp = local.timestamp
   }
+}
+
+output timestamp {
+    value = local.timestamp
 }
 
 output random_string {
